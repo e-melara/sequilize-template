63437b6a115e9d370d78e485e0e7daa6c022217d
Edwin Melara <edwin.melara.landaverde@gmail.com>
2022-09-17 12:17:05 -0600

    Add type modules

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require('express');
-
+import expres from "express";
 const app = express();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('listening on port 3000');
+  console.log("listening on port 3000");
 });
