2f59a744ff1ee538e0141eefab3d171b00847b25
Edwin Melara <edwin.melara.landaverde@gmail.com>
2022-09-18 17:28:24 -0600

    Mostrando details error

--- handler/error.js
+++ handler/error.js
@@ -1,8 +1,15 @@
 const handler = (err, req, res, next) => {
   if (err && err.error && err.error.isJoi) {
+    const { details } = err.error;
+    const messages = details.map(({  message, context }) =>{
+      return {
+        message,
+        field: context.key,
+      }
+    });
     res.status(400).json({
       type: err.type,
-      message: err.error,
+      message: messages,
     });
   } else {
     next(err);
